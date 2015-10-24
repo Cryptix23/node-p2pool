b599a013cf2b15b26bfd28ee2fdf55f4bdf5ae28
Joseph W Becher <jwbecher@drazisil.com>
2015-10-24 10:59:02 -0400

    gulp, not grunt

---
+++ Gulpfile.js
@@ -0,0 +1,34 @@
+'use strict'
+
+var gulp = require('gulp')
+// Start of the actual gulp
+
+var standard = require('gulp-standard')
+
+gulp.task('standard', function () {
+  return gulp.src(['./app.js'])
+    .pipe(standard())
+    .pipe(standard.reporter('default', {
+      breakOnError: true
+    }))
+})
+
+var mocha = require('gulp-mocha')
+
+gulp.task('mocha', function () {
+  return gulp.src('test/*', {read: false})
+    // gulp-mocha needs filepaths so you can't have any plugins before it
+    .pipe(mocha({reporter: 'nyan'}))
+})
+
+var coveralls = require('gulp-coveralls')
+
+// coveralls task
+gulp.task('coveralls', function () {
+  gulp.src('test/coverage/**/lcov.info')
+    .pipe(coveralls())
+})
+
+gulp.task('run', ['standard', 'mocha', 'coveralls'])
+
+gulp.task('default', ['run'])
