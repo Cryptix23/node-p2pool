ceeafa2e2e2915a08379909ff6734bdfe1739cfc
Joseph W. Becher <drazisil@users.noreply.github.com>
2015-10-24 21:57:13 -0400

    gulp-istanbul

--- Gulpfile.js
+++ Gulpfile.js
@@ -21,6 +21,27 @@ gulp.task('mocha', function () {
     .pipe(mocha({reporter: 'mocha-lcov-reporter'}))
 })
 
+var istanbul = require('gulp-istanbul');
+// We'll use mocha in this example, but any test framework will work
+var mocha = require('gulp-mocha');
+
+gulp.task('pre-test', function () {
+  return gulp.src(['lib/**/*.js'])
+    // Covering files
+    .pipe(istanbul())
+    // Force `require` to return covered files
+    .pipe(istanbul.hookRequire());
+});
+
+gulp.task('test', ['pre-test'], function () {
+  return gulp.src(['test/*.js'])
+    .pipe(mocha())
+    // Creating the reports after tests ran
+    .pipe(istanbul.writeReports())
+    // Enforce a coverage of at least 90%
+    .pipe(istanbul.enforceThresholds({ thresholds: { global: 90 } }));
+});
+
 var coveralls = require('gulp-coveralls')
 
 // coveralls task
@@ -29,6 +50,6 @@ gulp.task('coveralls', function () {
     .pipe(coveralls())
 })
 
-gulp.task('run', ['standard', 'mocha', 'coveralls'])
+gulp.task('run', ['standard', 'test', 'coveralls'])
 
 gulp.task('default', ['run'])
